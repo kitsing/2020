e78d84d5a1dd96dc5cbbe9a8a7d56a1b468f7599
Chu-Cheng Lin <chuchenglin@cmu.edu>
2021-11-08 11:56:36 -0500

    by type evaluation

--- task1/baselines/fst/disambiguate.py
+++ task1/baselines/fst/disambiguate.py
@@ -22,19 +22,27 @@ def main():
                 os.makedirs(f'{args.output_path_prefix}/{chunk}/')
             fname = f'{args.input_path_prefix}/{lang}/lexicons/{lang}.translit.sampled.{chunk}.tsv'
             output_fname = f'{args.output_path_prefix}/{chunk}/{lang}_{chunk}.tsv'
+            output_fname_unrepeated = f'{args.output_path_prefix}/{chunk}/{lang}_{chunk}.type.tsv'
             buffer = []
+            unrepeated_buffer = []
             with open(fname) as fh:
                 for l in fh:
                     splitted = l.split('\t')
                     assert len(splitted) == 3
                     times = int(splitted[2])
                     space_delimited_romanization = ' '.join(splitted[1])
+                    to_append = f'{splitted[0]}\t{space_delimited_romanization}\n'
+                    unrepeated_buffer.append(to_append)
                     for _ in range(times):
-                        buffer.append(f'{splitted[0]}\t{space_delimited_romanization}\n')
+                        buffer.append(to_append)
             if args.shuffle:
                 import numpy as np
                 rng = np.random.default_rng(args.seed)
                 rng.shuffle(buffer)
+
+            with open(output_fname_unrepeated, mode='w') as t_fh:
+                for l in unrepeated_buffer:
+                    fh.write(l)
 
             with open(output_fname, mode='w') as fh:
                 for l in buffer:
